3582080471780fe72864243f079eee171d140464
Roman Dybach <57605002+stakansoljarki@users.noreply.github.com>
2020-08-05 19:24:03 +0300

    Developer (#1)
    * add header, profile-card and privacy-button
    
    * fix css-comb
    
    * add card, scripts, media queries
    
    * minor edit
    
    Co-authored-by: roman dybach <roman@MacBook-Pro-roman.local>

---
+++ src/components/Privacy/privacy-button.js
@@ -0,0 +1,10 @@
+function privacyButton() {
+  document.querySelector(".privacy__button").addEventListener("click", (_) => {
+    document
+      .querySelector(".privacy__button")
+      .classList.toggle("privacy__button--active");
+  });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  privacyButton();
+});
